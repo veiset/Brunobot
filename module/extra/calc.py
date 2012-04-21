e682048ce9557d2dd39d9a833d19ae973f906847
Vegard Veiset <veiset@gmail.com>
2012-04-21 16:32:12 +0200

    Fixed calc. Using eval() without any form for checks is a VERY bad idea.

--- module/extra/calc.py
+++ module/extra/calc.py
@@ -7,15 +7,50 @@ cmd     = ['calc', 'c', 'gc']
 usage   = 'calc 5+5'
 description = 'Calculates something something...'
 
+import ast
 from math import *
+
+# >>> import math
+# >>> pythonMath = dir(math)
+pythonMath = ['acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 
+              'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 
+              'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod',
+              'frexp', 'fsum', 'gamma', 'hypot', 'isinf', 'isnan', 'ldexp',
+              'lgamma', 'log', 'log10', 'log1p', 'modf', 'pi', 'pow', 'radians',
+              'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'trunc']
+
+class visitor(ast.NodeVisitor):
+
+    def visit_Module(self, node):
+        self.functions = list()
+        self.generic_visit(node)
+        return self.functions
+
+    def generic_visit(self, node):
+        ast.NodeVisitor.generic_visit(self, node)
+
+    def visit_Name(self, node): 
+        self.functions.append(node.id)
+
+def legalExpr(expr):
+    expr = expr.lower()
+    try:
+        functions = visitor().visit(ast.parse(expr))
+        for f in functions:
+            if not f in pythonMath:
+                return False
+        return expr
+    except:
+        return False 
+
 
 def main(data):
     argv = data['argv']
     if argv:
-        result = 0
-        try:
+        expr = "".join(argv)
+        if legalExpr(expr):
             result = str(eval("".join(argv)))
-        except:
+        else:
             result = "error"
 
         communication.say(data['channel'],'Result: %s' % result)
