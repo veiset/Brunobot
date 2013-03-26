d2ec8860c1571845efd1782d0dc99074b7d3682c
Vegard Veiset <veiset@gmail.com>
2013-03-27 00:33:48 +0100

    Revert "Started working on recorder module."
    This reverts commit 0b13b305c0beb11ea9a7c693d0cd1acec05b32ea.

--- api/recorder/README.md
+++
@@ -1,3 +0,0 @@
-Module for recording and replaying events.
-
-
--- brunobot.py
+++ brunobot.py
@@ -1,15 +1,11 @@
 from pyric import irc
-from pyric import events
-import pyric.test.mocks as mocks
 
 import api.manager
-import ast
-
 
 class Brunobot():
     def __init__(self):
         #self.irc= irc.Instance('vzbot', 'vz', 'vz', 'irc.homelien.no', 6667)
-        self.nick = 'vzbotz' 
+        self.nick = 'vzbot' 
 
         self.irc = irc.Instance(self.nick, 'vz', 'vz', 'irc.codetalk.io', 6667)
         self.api = api.manager.Manager(self)
@@ -25,27 +21,11 @@ class Brunobot():
         self.module.add('module.ppoker.planningpoker', 'ppoker')
 
         self.irc.addListener('266', self.joinChannels)
-        self.irc.addListener('*', self.allEvents)
 
     def joinChannels(self, event):
         self.irc.join('#tlob')
-        #raise Exception("NOP!")
-
-    def allEvents(self, event):
-        print(event.data)
-        with open('lobby.log', 'a') as log:
-            log.write("%s\n" % str(event.data))
-
-    def playback(self):
-        with open('lobby.log', 'r') as log:
-            for line in log.readlines():
-                event = events.Event((ast.literal_eval(line)))
-                bot.irc.event(event)
-
 
 bot = Brunobot()
-bot.irc.irc = mocks.Socket()
-bot.playback()
-#bot.irc.connect()
+bot.irc.connect()
 
 #bot.irc.join('#brbot')
